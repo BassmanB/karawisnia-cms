ced7cf4672d53cdba290cda0fcd34cc571760187
Daniel Czarnozynski <daniel.czarnozynski@gmail.com>
2025-07-29 13:24:06 +0200

    Use categories from selected list

--- schemaTypes/projects.ts
+++ schemaTypes/projects.ts
@@ -3,11 +3,25 @@ export default {
   name: 'project',
   type: 'document',
   fields: [
-    { name: 'title', title: 'Title', type: 'string' },
+    { name: 'title', title: 'title', type: 'string' },
     { name: 'projectOverviewTitle', title: "description title", type: 'string' },
     { name: 'description', title: 'Description', type: 'text' },
     { name: 'projectOverview', title: 'Overview', type: 'array', of: [{ type: 'block' }] },
-    { name: 'categories', title: 'Categories', type: 'array', of: [{ type: 'string' }] },
+    { name: 'categories', title: 'Categories', type: 'array', of: [{ type: 'string' }],
+      options:{
+        list: [
+            { title: "all", value: "all" },
+            { title: "branding", value: "branding"},
+            { title: "exhibition", value: "exhibition" },
+            { title: "logo", value: "logo" },
+            { title: "poster", value: "poster" },
+            { title: "information", value: "information" },
+            { title: "typography", value: "typography" },
+            { title: "ux", value: "ux" },
+            { title: "ui", value: "ui" },
+            { title: "workshops", value: "workshops" }
+        ]
+    }},
     {
       name: 'images',
       title: 'Images',
@@ -42,9 +56,8 @@ export default {
     { name: 'designDetailsTitle', title: 'Section 2 title', type: 'string'},
     { name: 'designApproachTitle', title: "description title 2", type: 'string' },
     { name: 'designApproach', title: "descritpion 2", type: 'text' },
-    { name: 'videoId', title: 'Video', type: 'string'},
-    { name: 'tags', title: 'Tags', type: 'array', of: [{ type: 'string' }] },
-    { name: 'id', title: 'ID', type: 'string' },
+    { name: 'vtitleeotitle', title: 'Vtitleeo', type: 'string'},
+    { name: 'tags', title: 'Tags', type: 'array', of: [{ type: 'string' }] }
   ],
 }
 
